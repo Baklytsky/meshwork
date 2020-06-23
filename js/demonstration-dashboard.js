2e908e2c20092e41ef493b560f75420300009719
Baklytsky <Baklytsky@gmail.com>
2020-06-23 18:13:41 +0300

    fix pages

--- js/demonstration-dashboard.js
+++ js/demonstration-dashboard.js
@@ -9,8 +9,20 @@ myAccountLink.addEventListener('click', openSubMenu);
 
 function openSubMenu() {
     subMenuBlock.classList.toggle('active-sub-menu');
+    outBlockClick();
 }
+//------------- CLOSE Sub menu on click "My account" -----------------------------------
 
+function outBlockClick() {
+    if (subMenuBlock.classList.contains('active-sub-menu')) {
+        document.body.addEventListener('mousedown', function (e) {
+            if (e.target === subMenuBlock && e.target !== myAccountLink) {
+                console.log (e.target)
+                subMenuBlock.classList.remove('active-sub-menu');
+            }
+        })
+    }
+}
 //--------------------------- Pop-up on click sub menu links -----------------
 
 let faq = document.querySelector('.faq'),
@@ -32,7 +44,10 @@ subMenuLinks.forEach(i => i.addEventListener('click', () => {
     pop.style.display = 'block'
     xClick(pop);
     gotItClick(pop);
+    outPopUpClick(pop)
 }));
+
+//------------- CLOSE Pop-up`s -----------------------------------
 
 function xClick(pop) {
     closeX.forEach(i => i.addEventListener('click', () => {
@@ -44,6 +59,15 @@ function gotItClick(pop) {
     gotIt.forEach(i => i.addEventListener('click', () => {
         pop.style.display = "none";
     }));
+}
+function outPopUpClick(pop) {
+    if (pop.style.display === "block") {
+        document.body.addEventListener('mousedown', function (e) {
+            if (e.target === pop) {
+                pop.style.display = "none";
+            }
+        })
+    }
 }
 
 supportSubmit.addEventListener('click', ()=> {
