6a563a4e24c8b0d687fd88a1af8a5b11405b433d
Baklytsky <Baklytsky@gmail.com>
2020-07-06 13:53:17 +0300

    add brief summary page

--- js/demonstration-create-brief.js
+++ js/demonstration-create-brief.js
@@ -87,22 +87,53 @@ jQuery(function ($) {
         });
     });
 });
-//----------------------------------- Date picker ----------------------------------------
+//----------------------------------- add New Attachment ----------------------------------------
+let addNewAttachment = $(".addNewAttachment"),
+    attachmentNum = $("#attachmentNum"),
+    msg = $(".Msg");
+
+    addNewAttachment.on("click", addAttachment);
 
 //--------------------------------------------------------------remove attachment
-    function clearAttachment(field) {
-        let name = $(field).closest("div").find("input[type = 'file']").attr("name").substr(5);
-        $(field).closest("div").remove();
-        let strNew = $("#attachmentNum").val().toString();
-        strNew = strNew + name;
-        $("#attachmentNum").val(strNew);
+function clearAttachment(field) {
+    msg.slideUp("fast");
+    let name = $(field).closest("div").find("input[type = 'file']").attr("name").substr(5);
+    $(field).closest("div").remove();
+    let strNew = attachmentNum.val().toString();
+    strNew = strNew + name;
+    attachmentNum.val(strNew);
+    let num = attachmentNum.val().length;
+    if (num == 6) {
+        $(".AttachmentFS .sureAdd").slideUp(500);
     }
+}
 
 //--------------------------------------------------------------add attachment
-    function addAttachment(field) {
-        let str = $("#attachmentNum").val().toString();
-        let id = str.charAt(0).toString();
-        $(".attachmentContainer").append("<div class='file-block'>" + "<input type='file' class='input-file' name='file" + id + "'/><input type='button' value='X' class='GeneralButton x' onclick='clearAttachment(this)' /> </div>");
+function addAttachment(field) {
+    msg.slideUp("fast");
+    $(".AttachmentFS .sureAdd").slideDown(500);
+    let num = attachmentNum.val().length;
+    if (num < 7 && num > 0) {
+        let str = attachmentNum.val().toString(),
+            id = str.charAt(0).toString(),
+            inputId = '#' + 'Image' + id;
+        $(".attachmentContainer").append("<div class='file-block'>" + "<label for='Image" + id + "' class='input-file-label'>Click to add file</label></label><input type='file' class='input-file' id='Image" + id + "' name='Image" + id + "'/><span class='input-close' onclick='clearAttachment(this)'></span> </div>");
+        $(inputId).change(function() {
+            let file = $(inputId)[0].files[0].name,
+                format = file.split(".")
+            $(this).prev('label').text(file);
+            $(this).prev('label').append("<img src='../img/append-" + format[1] + ".png' alt='Image icon' class='append-image'/>");
+            $(".formats").append(format[1] + ', ' )
+        });
         let temp = str.substr(1);
-        $("#attachmentNum").val(temp);
+        attachmentNum.val(temp);
+
     }
+    else {
+        //view message
+        msg.slideDown("fast");
+    }
+}
+
+
+
